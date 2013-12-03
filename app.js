43d15db942968e37eb053882ecf346fd5d319361
Pomin Wu <pomin5@gmail.com>
2013-12-04 02:01:57 +0800

    add angular controller

---
+++ app.js
@@ -0,0 +1,77 @@
+
+var epaIncineratorApp = angular.module("epaIncineratorApp", []);
+
+epaIncineratorApp.controller("ChartCtrl", function($scope, $http) {
+  $scope.data = [];
+  $scope.reloadJSON = function() {
+    $http.get("incinerator-air-polution.json")
+    .success(function(data) {
+      $scope.data = data;
+    })
+    .error(function(data, status) {
+      if (status == 404) {
+        $scope.error = "Not found";
+      } else {
+        $scope.error = "Error: " + status;
+      }
+    });
+  };
+});
+
+epaIncineratorApp.directive('epaVisual', function() {
+
+  var width = 960,
+    barHeight = 25;
+
+  return {
+    restrict: "E",
+    scope: {
+      data: '='
+    },
+    link: function(scope, element, attrs) {
+      var x = d3.scale.linear()
+        .range([0, width]);
+
+      scope.$watch('data', function(newVal, oldVal) {
+        var data = newVal;
+
+        if (!data) {
+          return;
+        }
+
+        var chart = d3.select("epa-visual")
+          .append("svg")
+          .attr("class", "chart")
+          .attr("width", width);
+
+        x.domain([0, d3.max(data, function(d) { return +d.NOx; })]);
+
+        chart.attr("height", barHeight * data.length);
+
+        var bar = chart.selectAll("g")
+          .data(data)
+          .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+        bar.append("rect")
+          .attr("class", "bar")
+          .attr("width", function(d) { return x(+d.NOx); })
+          .attr("height", barHeight - 1);
+
+        bar.append("text")
+          .attr("x", function(d) { return x(+d.NOx) - d.NOx.length * 9; })
+          .attr("y", barHeight / 2)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.NOx; });
+
+        bar.append("text")
+          .attr("x", 0)
+          .attr("y", barHeight / 2)
+          .attr("dx", ".2em")
+          .attr("dy", ".35em")
+          .text(function(d) { return d.IncineratorName + " (" + d.ReportDate + ")"; });
+      });
+
+    }
+  };
+});
