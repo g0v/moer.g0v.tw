0713fb50a6750bfe84b91624e106a07367008db8
Pomin Wu <pomin5@gmail.com>
2013-12-04 23:30:46 +0800

    add substance selection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var epaIncineratorApp = angular.module("epaIncineratorApp", []);
 
 epaIncineratorApp.controller("ChartCtrl", function($scope, $http) {
   $scope.data = [];
+  $scope.substance = 'NOx';
 
   $http.get("incinerator-air-polution.json")
   .success(function(data) {
@@ -20,72 +21,87 @@ epaIncineratorApp.controller("ChartCtrl", function($scope, $http) {
 epaIncineratorApp.directive('epaVisual', function() {
 
   var width = 960,
-    barHeight = 25;
+      barHeight = 25;
+
+  var x = d3.scale.linear()
+    .range([0, width]);
+
+  var chart = d3.select("epa-visual")
+    .append("svg")
+    .attr("class", "chart")
+    .attr("width", width);
+
+  function drawBarChart(data, substance = "NOx") {
+
+    var scales = {
+      "NOx": 200,
+      "SOx": 20
+    };
+
+    x.domain([0, scales[substance]]);
+
+    var barSelection = chart.selectAll("g")
+      .data(data);
+
+    barSelection.select("rect")
+      .attr("width", function(d) { return x(+d[substance]); });
+    barSelection.select("text.substance-value")
+      .attr("x", function(d) { return x(+d[substance]) - d[substance].length * 9; })
+      .text(function(d) { return d[substance]; });
+
+    barSelection.select("text.incinerator-name")
+      .text(function(d) { return d.IncineratorName + " (" + d.ReportDate + ")"; });
+
+    var bar = barSelection
+      .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+      .attr("class", "bar")
+      .attr("width", function(d) { return x(+d[substance]); })
+      .attr("height", barHeight - 1);
+
+    bar.append("text")
+      .attr("class", "substance-value")
+      .attr("x", function(d) { return x(+d[substance]) - d[substance].length * 9; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d[substance]; });
+
+    bar.append("text")
+      .attr("class", "incinerator-name")
+      .attr("x", 0)
+      .attr("y", barHeight / 2)
+      .attr("dx", ".2em")
+      .attr("dy", ".35em")
+      .text(function(d) { return d.IncineratorName + " (" + d.ReportDate + ")"; });
+
+    barSelection.exit()
+        .remove();
+  }
 
   return {
     restrict: "E",
     scope: {
-      data: '='
+      data: '=',
+      substance: '='
     },
     link: function(scope, element, attrs) {
-      var x = d3.scale.linear()
-        .range([0, width])
-        .domain([0, 150]);
-
-      var chart = d3.select("epa-visual")
-        .append("svg")
-        .attr("class", "chart")
-        .attr("width", width);
-
       scope.$watch('data', function(newVal, oldVal) {
         var data = newVal;
-
         if (!data) {
           return;
         }
-
         chart.attr("height", barHeight * data.length);
-
-        var barSelection = chart.selectAll("g")
-          .data(data);
-
-        barSelection.select("rect")
-          .attr("width", function(d) { return x(+d.NOx); });
-        barSelection.select("text.substance-value")
-          .attr("x", function(d) { return x(+d.NOx) - d.NOx.length * 9; })
-          .text(function(d) { return d.NOx; });
-
-        barSelection.select("text.incinerator-name")
-          .text(function(d) { return d.IncineratorName + " (" + d.ReportDate + ")"; });
-
-        var bar = barSelection
-          .enter().append("g")
-          .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-        bar.append("rect")
-          .attr("class", "bar")
-          .attr("width", function(d) { return x(+d.NOx); })
-          .attr("height", barHeight - 1);
-
-        bar.append("text")
-          .attr("class", "substance-value")
-          .attr("x", function(d) { return x(+d.NOx) - d.NOx.length * 9; })
-          .attr("y", barHeight / 2)
-          .attr("dy", ".35em")
-          .text(function(d) { return d.NOx; });
-
-        bar.append("text")
-          .attr("class", "incinerator-name")
-          .attr("x", 0)
-          .attr("y", barHeight / 2)
-          .attr("dx", ".2em")
-          .attr("dy", ".35em")
-          .text(function(d) { return d.IncineratorName + " (" + d.ReportDate + ")"; });
-
-        barSelection.exit()
-            .remove();
+        drawBarChart(data, scope.substance);
       });
-
+      scope.$watch('substance', function(newVal, oldVal) {
+        if (!newVal) {
+          return;
+        }
+        scope.substance = newVal;
+        drawBarChart(scope.data, newVal);
+      });
     }
   };
 });
